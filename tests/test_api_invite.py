4fb0b8db2ff24449fa30a60575b43ef5727903c4
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-03-08 12:56:33 +0100

    Add device claim to test_claim_ok

--- tests/test_api_invite.py
+++ tests/test_api_invite.py
@@ -28,29 +28,48 @@ async def device_invitation(authenticated_client):
 
 
 @pytest.mark.trio
-async def test_claim_user(test_app, local_device, authenticated_client, user_invitation):
+@pytest.mark.parametrize("type", ["user", "device"])
+async def test_claim_ok(test_app, local_device, authenticated_client, type):
     claimer_client = test_app.test_client()
-    claimer_email = "bob@example.com"
     greeter_sas_available = trio.Event()
     greeter_sas = None
     claimer_sas_available = trio.Event()
     claimer_sas = None
-    claimer_key = b"P@ssw0rd."
+
+    # First create the invitation
+    if type == "user":
+        claimer_email = "bob@example.com"
+        response = await authenticated_client.post(
+            "/invitations", json={"type": "user", "claimer_email": claimer_email}
+        )
+        body = await response.get_json()
+        assert response.status_code == 200
+        invitation = InvitationInfo("user", claimer_email, body["token"])
+    else:
+        response = await authenticated_client.post("/invitations", json={"type": "device"})
+        body = await response.get_json()
+        assert response.status_code == 200
+        invitation = InvitationInfo("device", None, body["token"])
+
+    new_device_email = (
+        invitation.claimer_email if type == "user" else local_device.device.human_handle.email
+    )
+    new_device_key = b"P@ssw0rd."
 
     async def _claimer():
         nonlocal claimer_sas
 
         # Step 0
         response = await claimer_client.post(
-            f"/invitations/{user_invitation.token}/claimer/0-retreive-info", json={}
+            f"/invitations/{invitation.token}/claimer/0-retreive-info", json={}
         )
         body = await response.get_json()
         assert response.status_code == 200
-        assert body == {"type": "user", "greeter_email": local_device.device.human_handle.email}
+        assert body == {"type": type, "greeter_email": local_device.device.human_handle.email}
 
         # Step 1
         response = await claimer_client.post(
-            f"/invitations/{user_invitation.token}/claimer/1-wait-peer-ready", json={}
+            f"/invitations/{invitation.token}/claimer/1-wait-peer-ready", json={}
         )
         body = await response.get_json()
         assert response.status_code == 200
@@ -61,7 +80,7 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
 
         # Step 2
         response = await claimer_client.post(
-            f"/invitations/{user_invitation.token}/claimer/2-check-trust",
+            f"/invitations/{invitation.token}/claimer/2-check-trust",
             json={"greeter_sas": greeter_sas},
         )
         body = await response.get_json()
@@ -72,7 +91,7 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
 
         # Step 3
         response = await claimer_client.post(
-            f"/invitations/{user_invitation.token}/claimer/3-wait-peer-trust", json={}
+            f"/invitations/{invitation.token}/claimer/3-wait-peer-trust", json={}
         )
         body = await response.get_json()
         assert response.status_code == 200
@@ -80,8 +99,8 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
 
         # Step 4
         response = await claimer_client.post(
-            f"/invitations/{user_invitation.token}/claimer/4-finalize",
-            json={"key": b64encode(claimer_key).decode("ascii")},
+            f"/invitations/{invitation.token}/claimer/4-finalize",
+            json={"key": b64encode(new_device_key).decode("ascii")},
         )
         body = await response.get_json()
         assert response.status_code == 200
@@ -92,17 +111,20 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
 
         # Step 1
         response = await authenticated_client.post(
-            f"/invitations/{user_invitation.token}/greeter/1-wait-peer-ready", json={}
+            f"/invitations/{invitation.token}/greeter/1-wait-peer-ready", json={}
         )
         body = await response.get_json()
         assert response.status_code == 200
-        assert body == {"type": "user", "greeter_sas": ANY}
+        if type == "user":
+            assert body == {"type": "user", "greeter_sas": ANY}
+        else:
+            assert body == {"type": "device", "greeter_sas": ANY}
         greeter_sas = body["greeter_sas"]
         greeter_sas_available.set()
 
         # Step 2
         response = await authenticated_client.post(
-            f"/invitations/{user_invitation.token}/greeter/2-wait-peer-trust", json={}
+            f"/invitations/{invitation.token}/greeter/2-wait-peer-trust", json={}
         )
         body = await response.get_json()
         assert response.status_code == 200
@@ -113,7 +135,7 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
 
         # Step 3
         response = await authenticated_client.post(
-            f"/invitations/{user_invitation.token}/greeter/3-check-trust",
+            f"/invitations/{invitation.token}/greeter/3-check-trust",
             json={"claimer_sas": claimer_sas},
         )
         body = await response.get_json()
@@ -121,9 +143,12 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
         assert body == {}
 
         # Step 4
+        if type == "user":
+            json = {"granted_profile": "ADMIN", "claimer_email": invitation.claimer_email}
+        else:
+            json = {"granted_profile": "ADMIN"}
         response = await authenticated_client.post(
-            f"/invitations/{user_invitation.token}/greeter/4-finalize",
-            json={"granted_profile": "ADMIN", "claimer_email": claimer_email},
+            f"/invitations/{invitation.token}/greeter/4-finalize", json=json
         )
         body = await response.get_json()
         assert response.status_code == 200
@@ -141,12 +166,13 @@ async def test_claim_user(test_app, local_device, authenticated_client, user_inv
     assert body == {"users": [], "device": None}
 
     # # And the new user should be visible
-    # response = await authenticated_client.get("/humans")
-    # body = await response.get_json()
-    # assert response.status_code == 200
+    # if type == "user":
+    #     response = await authenticated_client.get("/humans")
+    #     body = await response.get_json()
+    #     assert response.status_code == 200
 
     # New user should be able to connect
     response = await claimer_client.post(
-        "/auth", json={"email": claimer_email, "key": b64encode(claimer_key).decode("ascii")}
+        "/auth", json={"email": new_device_email, "key": b64encode(new_device_key).decode("ascii")}
     )
     assert response.status_code == 200
