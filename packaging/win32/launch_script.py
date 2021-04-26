b71375d48aba6867c1f4212ddb1e5e25efc3a103
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-04-26 11:24:33 +0200

    Enable logging in win32 packaged application

--- packaging/win32/launch_script.py
+++ packaging/win32/launch_script.py
@@ -1,5 +1,16 @@
 # Resana Secure Copyright (c) 2021 Scille SAS
 
+import os
+import sys
+from pathlib import Path
+
 from resana_secure.cli import run_cli
 
+if not sys.argv[1:]:
+    sys.argv += [
+        "--log-level",
+        "WARNING",
+        "--log-file",
+        str(Path(os.environ["APPDATA"]).joinpath("resana_secure/resana_secure.log").absolute()),
+    ]
 run_cli()
