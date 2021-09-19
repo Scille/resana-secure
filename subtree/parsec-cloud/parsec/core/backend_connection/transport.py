e19d340764258093124bf8ebe450b387752354e2
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-09-19 15:18:42 +0200

    Add http/https proxy support

--- subtree/parsec-cloud/parsec/core/backend_connection/transport.py
+++ subtree/parsec-cloud/parsec/core/backend_connection/transport.py
@@ -4,10 +4,14 @@ import os
 import trio
 import ssl
 import certifi
-from async_generator import asynccontextmanager
+import h11
 from structlog import get_logger
 from typing import Optional
 from parsec.api.protocol.handshake import HandshakeOutOfBallparkError
+from base64 import b64encode
+from async_generator import asynccontextmanager
+from urllib.request import getproxies, proxy_bypass
+from urllib.parse import urlsplit, SplitResult
 
 from parsec.crypto import SigningKey
 from parsec.api.transport import Transport, TransportError, TransportClosedByPeer
@@ -78,14 +82,17 @@ async def _connect(
     keepalive: Optional[int],
     handshake: BaseClientHandshake,
 ) -> Transport:
-    try:
-        stream = await trio.open_tcp_stream(hostname, port)
+    stream = await _maybe_connect_through_proxy(hostname, port, use_ssl)
 
-    except OSError as exc:
-        logger.warning(
-            "Impossible to connect to backend", hostname=hostname, port=port, exc_info=exc
-        )
-        raise BackendNotAvailable(exc) from exc
+    if not stream:
+        try:
+            stream = await trio.open_tcp_stream(hostname, port)
+
+        except OSError as exc:
+            logger.warning(
+                "Impossible to connect to backend", hostname=hostname, port=port, exc_info=exc
+            )
+            raise BackendNotAvailable(exc) from exc
 
     if use_ssl:
         stream = _upgrade_stream_to_ssl(stream, hostname)
@@ -114,7 +121,106 @@ async def _connect(
     return transport
 
 
-def _upgrade_stream_to_ssl(raw_stream, hostname):
+def cook_basic_auth_header(username: str, password: str) -> str:
+    step1 = f"{username}:{password}".encode("utf8")
+    step2 = b64encode(step1).decode("ascii")
+    return f"Basic {step2}"
+
+
+async def _maybe_connect_through_proxy(
+    hostname: str, port: int, use_ssl: bool
+) -> Optional[trio.abc.Stream]:
+    # Proxy config is accessed two times here: first to check proxy bypass,
+    # then to retrieve the proxy url. This is okay enough given proxy config
+    # is not supposed to change that often.
+
+    if proxy_bypass(hostname):
+        return None
+
+    proxy_type = "https" if use_ssl else "http"
+    proxy_url = getproxies().get(proxy_type)
+    if not proxy_url:
+        return None
+    try:
+        proxy = urlsplit(proxy_url)
+        # Typing helper, as result could be SplitResultBytes if we have provided bytes instead of str
+        assert isinstance(proxy, SplitResult)
+    except ValueError:
+        # Invalid url
+        return None
+    if proxy.port:
+        proxy_port = proxy.port
+    else:
+        proxy_port = 443 if proxy.scheme == "https" else 80
+    if not proxy.hostname:
+        return None
+    proxy_headers = []
+    if proxy.username is not None and proxy.password is not None:
+        proxy_headers.append(
+            ("Proxy-Authorization", cook_basic_auth_header(proxy.username, proxy.password))
+        )
+
+    # Connect to the proxy
+
+    stream: trio.abc.Stream
+    try:
+        stream = await trio.open_tcp_stream(proxy.hostname, proxy_port)
+
+    except OSError as exc:
+        logger.warning("Impossible to connect to proxy", reason=exc)
+        raise BackendNotAvailable(exc) from exc
+
+    if proxy.scheme == "https":
+        stream = _upgrade_stream_to_ssl(stream, proxy.hostname)
+
+    # Ask the proxy to connect the actual host
+
+    conn = h11.Connection(our_role=h11.CLIENT)
+
+    async def send(event):
+        data = conn.send(event)
+        await stream.send_all(data)
+
+    async def next_event():
+        while True:
+            event = conn.next_event()
+            if event is h11.NEED_DATA:
+                data = await stream.receive_some(2048)
+                conn.receive_data(data)
+                continue
+            return event
+
+    host = f"{hostname}:{port}"
+    try:
+        await send(
+            h11.Request(
+                method="CONNECT",
+                target=host,
+                headers=[
+                    # According to RFC7230 (https://datatracker.ietf.org/doc/html/rfc7230#section-5.4)
+                    # Client must provide Host header, but the proxy must replace it
+                    # with the host information of the request-target. So in theory
+                    # we could set any dummy value for the Host header here !
+                    ("Host", host),
+                    *proxy_headers,
+                ],
+            )
+        )
+        answer = await next_event()
+        if not isinstance(answer, h11.Response) or not 200 <= answer.status_code < 300:
+            logger.warning("Bad answer from proxy", answer=answer, target_host=host)
+            raise BackendNotAvailable("Bad answer from proxy")
+
+    except trio.BrokenResourceError as exc:
+        logger.warning(
+            "Proxy has unexpectedly closed the connection", exc_info=exc, target_host=host
+        )
+        raise BackendNotAvailable("Proxy has unexpectedly closed the connection") from exc
+
+    return stream
+
+
+def _upgrade_stream_to_ssl(raw_stream: trio.abc.Stream, hostname: str) -> trio.abc.Stream:
     # The ssl context should be generated once and stored into the config
     # however this is tricky (should ssl configuration be stored per device ?)
 
