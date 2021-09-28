65de378ca73b4868c62792c94f4024501e935216
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-09-28 21:29:00 +0200

    Add config file support and proxy related params in cli

--- packaging/win32/launch_script.py
+++ packaging/win32/launch_script.py
@@ -2,15 +2,26 @@
 
 import os
 import sys
+import json
 from pathlib import Path
 
 from resana_secure.cli import run_cli
 
-if not sys.argv[1:]:
-    sys.argv += [
-        "--log-level",
-        "WARNING",
-        "--log-file",
-        str(Path(os.environ["APPDATA"]).joinpath("resana_secure/resana_secure.log").absolute()),
-    ]
-run_cli()
+
+config_dir = (Path(os.environ["APPDATA"]) / "resana_secure").absolute()
+log_file = config_dir / "resana_secure.log"
+
+# Config file is just a convoluted way of passing params to sys.argv
+args = sys.argv[1:]
+try:
+    conf = json.loads((config_dir / "config.json").read_text())
+    if isinstance(conf, dict):
+        for key, value in conf.items():
+            if isinstance(key, str) and isinstance(value, str):
+                args.append(f"--{key.replace('_', '-')}")
+                args.append(value)
+except (OSError, json.JSONDecodeError):
+    pass
+
+
+run_cli(args=args, default_log_level="WARNING", default_log_file=log_file)
--- resana_secure/cli.py
+++ resana_secure/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import Optional
+from typing import Optional, Tuple
 from pathlib import Path
 from functools import partial
 import os
@@ -9,6 +9,7 @@ import logging
 import structlog
 
 from parsec.core.config import CoreConfig
+from parsec.core.backend_connection.transport import force_proxy_url, force_proxy_pac_url
 
 from .app import serve_app
 
@@ -53,34 +54,81 @@ def _setup_logging(log_level: str, log_file: Optional[Path]) -> None:
         logging.basicConfig(format=format, datefmt=datefmt, stream=sys.stdout, level=level)
 
 
-def build_config(
-    config_dir: Optional[Path] = None,
-    data_base_dir: Optional[Path] = None,
-    cache_base_dir: Optional[Path] = None,
-) -> CoreConfig:
-    home = Path.home()
+def get_default_dirs() -> Tuple[Path, Path, Path, Path]:
     mountpoint_base_dir = Path.home() / "Resana-Secure"
 
     if os.name == "nt":
         appdata = Path(os.environ["APPDATA"])
-        data_base_dir = data_base_dir or appdata / "resana_secure/data"
-        cache_base_dir = cache_base_dir or appdata / "resana_secure/cache"
-        config_dir = config_dir or appdata / "resana_secure/config"
+        data_base_dir = appdata / "resana_secure/data"
+        cache_base_dir = appdata / "resana_secure/cache"
+        config_dir = appdata / "resana_secure/config"
 
     else:
+        home = Path.home()
+
         path = os.environ.get("XDG_DATA_HOME") or f"{home}/.local/share"
-        data_base_dir = data_base_dir or Path(path) / "resana_secure"
+        data_base_dir = Path(path) / "resana_secure"
 
         path = os.environ.get("XDG_CACHE_HOME") or f"{home}/.cache"
-        cache_base_dir = cache_base_dir or Path(path) / "resana_secure"
+        cache_base_dir = Path(path) / "resana_secure"
 
         path = os.environ.get("XDG_CONFIG_HOME") or f"{home}/.config"
-        config_dir = config_dir or Path(path) / "resana_secure"
+        config_dir = Path(path) / "resana_secure"
 
-    return CoreConfig(
+    return mountpoint_base_dir, data_base_dir, cache_base_dir, config_dir
+
+
+def run_cli(args=None, default_log_level: str = "INFO", default_log_file: Optional[Path] = None):
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("--port", type=int, default=5775)
+    parser.add_argument("--host", default="127.0.0.1")
+    parser.add_argument("--config", type=Path)
+    parser.add_argument("--data", type=Path)
+    parser.add_argument("--client-origin", type=lambda x: x.split(";"), default=["*"])
+    parser.add_argument(
+        "--resana-website-url",
+        type=_cook_website_url,
+        metavar="URL",
+        default="https://resana.numerique.gouv.fr/",
+    )
+    parser.add_argument("--disable-gui", action="store_true")
+    parser.add_argument("--disable-mountpoint", action="store_true")
+    parser.add_argument(
+        "--log-level", choices=("DEBUG", "INFO", "WARNING", "ERROR"), default=default_log_level
+    )
+    parser.add_argument("--log-file", type=Path, default=default_log_file)
+    parser.add_argument(
+        "--force-proxy", type=_cook_website_url, metavar="URL", help="Force use of proxy server"
+    )
+    parser.add_argument(
+        "--force-proxy-pac",
+        type=_cook_website_url,
+        metavar="URL",
+        help="Force use of server provided proxy .PAC configuration",
+    )
+    args = parser.parse_args(args=args)
+
+    if args.force_proxy and args.force_proxy_pac:
+        raise SystemExit("`--force-proxy-pac` and `--force-proxy` are mutually exclusive")
+    if args.force_proxy:
+        force_proxy_url(args.force_proxy)
+    if args.force_proxy_pac:
+        force_proxy_pac_url(args.force_proxy_pac)
+
+    (
+        mountpoint_base_dir,
+        default_data_base_dir,
+        cache_base_dir,
+        default_config_dir,
+    ) = get_default_dirs()
+    config_dir = args.config or default_config_dir
+    data_base_dir = args.data or default_data_base_dir
+
+    config = CoreConfig(
         config_dir=config_dir,
         data_base_dir=data_base_dir,
         cache_base_dir=cache_base_dir,
+        # Only used on linux (Windows mounts with drive letters)
         mountpoint_base_dir=mountpoint_base_dir,
         # Use a mock to disable mountpoint instead of relying on this option
         mountpoint_enabled=True,
@@ -88,24 +136,6 @@ def build_config(
         ipc_socket_file=data_base_dir / "resana-secure.lock",
         preferred_org_creation_backend_addr=None,
     )
-
-
-def run_cli():
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument("--port", type=int, default=5775)
-    parser.add_argument("--host", default="127.0.0.1")
-    parser.add_argument("--config-dir", type=Path)
-    parser.add_argument("--client-origin", type=lambda x: x.split(";"), default=["*"])
-    parser.add_argument(
-        "--resana-website-url", type=_cook_website_url, default="https://resana.numerique.gouv.fr/"
-    )
-    parser.add_argument("--disable-gui", action="store_true")
-    parser.add_argument("--disable-mountpoint", action="store_true")
-    parser.add_argument(
-        "--log-level", choices=("DEBUG", "INFO", "WARNING", "ERROR"), default="INFO"
-    )
-    parser.add_argument("--log-file", type=Path)
-    args = parser.parse_args()
 
     _setup_logging(args.log_level, args.log_file)
 
@@ -120,8 +150,6 @@ def run_cli():
             return _nop_runner
 
         manager.get_mountpoint_runner = _get_mountpoint_runner_mocked
-
-    config = build_config(config_dir=args.config_dir)
 
     trio_main = partial(
         serve_app,
