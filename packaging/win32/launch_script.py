f2dde696736de4e49fc7a26eac3d6af3738c06ee
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-09-30 00:41:08 +0200

    Add log in packaging/win32/launch_script.py to log error on config file

--- packaging/win32/launch_script.py
+++ packaging/win32/launch_script.py
@@ -13,15 +13,21 @@ log_file = config_dir / "resana_secure.log"
 
 # Config file is just a convoluted way of passing params to sys.argv
 args = sys.argv[1:]
+config_file_path = config_dir / "config.json"
 try:
-    conf = json.loads((config_dir / "config.json").read_text())
+    conf = json.loads(config_file_path.read_text())
     if isinstance(conf, dict):
         for key, value in conf.items():
             if isinstance(key, str) and isinstance(value, str):
                 args.append(f"--{key.replace('_', '-')}")
                 args.append(value)
-except (OSError, json.JSONDecodeError):
+except FileNotFoundError:
     pass
-
+except (OSError, json.JSONDecodeError) as exc:
+    try:
+        with open(log_file, "a") as fd:
+            fd.write(f"Ignoring invalid configuration file {config_file_path}: {repr(exc)}\n")
+    except Exception:
+        pass
 
 run_cli(args=args, default_log_level="WARNING", default_log_file=log_file)
