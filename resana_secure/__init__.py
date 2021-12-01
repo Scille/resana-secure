39f610d480b17e7c860c90eac182dc056199100e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-12-01 12:11:29 +0100

    Customize User-Agent for resana-secure

---
+++ resana_secure/__init__.py
@@ -0,0 +1,14 @@
+# Early monkey patches
+
+
+def _monkeypatch_user_agent():
+    from ._version import __version__
+    import parsec.api.transport
+    import parsec.core.backend_connection.transport
+
+    USER_AGENT = f"resana-secure/{__version__}"
+    parsec.api.transport.USER_AGENT = USER_AGENT
+    parsec.core.backend_connection.transport.USER_AGENT = USER_AGENT
+
+
+_monkeypatch_user_agent()
