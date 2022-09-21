6844d02d52bc07753cf951e0b4c28f348e8c361b
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2022-09-21 10:59:27 +0200

    Use manifest_unverified_load to load manifest

--- antivirus_connector/routes.py
+++ antivirus_connector/routes.py
@@ -5,7 +5,8 @@ import structlog
 from quart import Blueprint, request, current_app
 
 from parsec.sequester_crypto import sequester_service_decrypt
-from parsec.api.data import AnyRemoteManifest, FileManifest
+from parsec.api.data import FileManifest
+from parsec.api.data.manifest import manifest_unverified_load
 from parsec.api.protocol import OrganizationID
 
 from .utils import APIException
@@ -33,7 +34,7 @@ async def load_manifest(vlob):
             current_app.config["APP_CONFIG"].authority_private_key, vlob
         )
         # Connector does not care if data is signed or not
-        manifest = AnyRemoteManifest.unsecure_load(decrypted_vlob)
+        manifest = manifest_unverified_load(decrypted_vlob)
         if not isinstance(manifest, FileManifest):
             return None
         return manifest
