ac914c2b20829867abb9460be7e02234cc661c90
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-10-03 10:32:13 +0200

    Add antivirus-connector v0.10.0+dev

---
+++ antivirus_connector/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from dataclasses import dataclass
+from parsec.backend.config import BaseBlockStoreConfig
+
+
+@dataclass
+class AppConfig:
+    sequester_service_decryption_key: bytes
+    antivirus_api_url: str
+    antivirus_api_key: str
+    blockstore_config: BaseBlockStoreConfig
+    db_url: str
+    db_min_connections: int
+    db_max_connections: int
