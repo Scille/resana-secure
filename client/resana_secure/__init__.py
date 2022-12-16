9f9b42b881d4ffeba246d3e3eccf631e8b947eae
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2022-12-16 11:54:25 +0100

    Change icon depending on the backend address

--- client/resana_secure/__init__.py
+++ client/resana_secure/__init__.py
@@ -1,6 +1,11 @@
 # Early monkey patches
 
+import sys
+import os
 from typing import Any, cast
+from importlib.abc import Traversable
+
+from parsec.core.types import LocalDevice
 
 
 def _monkeypatch_parsec_version() -> None:
@@ -33,6 +38,29 @@ def _monkeypatch_greyed_dialog() -> None:
     cast(Any, GreyedDialog).paintEvent = _paint_event
 
 
+def _monkeypatch_drive_icon() -> None:
+    if sys.platform != "win32":
+        return
+
+    from parsec.core import win_registry
+
+    def _get_drive_icon_path(device: LocalDevice) -> Traversable:
+        from PyQt5.QtWidgets import QApplication
+        from .gui import ResanaGuiApp
+        from .cores_manager import is_org_hosted_on_rie
+
+        qt_app = cast(ResanaGuiApp, QApplication.instance())
+
+        icon = "quake.ico"
+        if is_org_hosted_on_rie(device.organization_addr, qt_app.config.rie_server_addrs):
+            icon = "doom.ico"
+
+        return cast(Traversable, os.path.join(os.getcwd(), icon))
+
+    win_registry._get_drive_icon_path = _get_drive_icon_path
+
+
 _monkeypatch_user_agent()
 _monkeypatch_parsec_version()
 _monkeypatch_greyed_dialog()
+_monkeypatch_drive_icon()
