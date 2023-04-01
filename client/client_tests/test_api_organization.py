994d3f9a65b1c154d362ea1b54a4c9c9f85b3ebb
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-04-01 12:12:22 +0200

    Correct client for parsec-cloud 2.15.1

--- client/client_tests/test_api_organization.py
+++ client/client_tests/test_api_organization.py
@@ -1,5 +1,4 @@
 import pytest
-import oscrypto.asymmetric as crypto
 import base64
 from pathlib import Path
 from quart.typing import TestAppProtocol
@@ -9,7 +8,7 @@ from parsec.backend import BackendApp
 from parsec.backend.organization import generate_bootstrap_token
 from parsec.core.local_device import list_available_devices
 from parsec.core.types import BackendAddr, BackendOrganizationBootstrapAddr
-from parsec.sequester_crypto import SequesterVerifyKeyDer
+from parsec._parsec import SequesterSigningKeyDer
 
 
 @pytest.fixture
@@ -281,8 +280,7 @@ async def test_bootstrap_organization_with_sequester_key(
     running_backend: BackendApp,
 ):
     test_client = test_app.test_client()
-    pub_key, _ = crypto.generate_pair("rsa", bit_size=1024)
-    seq_verify_key = SequesterVerifyKeyDer(pub_key)
+    _, seq_verify_key = SequesterSigningKeyDer.generate_pair(size_in_bits=1024)
 
     response = await test_client.post(
         "/organization/bootstrap",
