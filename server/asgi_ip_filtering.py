2dd2249e9a5646bf133abe365525c3cc89464bca
Vincent Michel <vxgmichel@gmail.com>
2023-09-21 22:02:31 +0200

    Add ROUTES_REQUIRING_FILTERING to AsgiIpFilteringMiddleware

--- server/asgi_ip_filtering.py
+++ server/asgi_ip_filtering.py
@@ -1,10 +1,18 @@
 """
 Add IP filtering to a hypercorn trio app.
 
-First configure the authorized networks using the `ASGI_AUTHORIZED_NETWORKS` environement
-variable:
+First configure the authorized networks using the following environement variables:
 
-    export ASGI_AUTHORIZED_NETWORKS="192.168.0.0/16 127.0.0.0/24"
+- `ASGI_AUTHORIZED_NETWORKS`: the ranges of allowed client IPs, as seen in the `x-real-ip` header
+- `ASGI_AUTHORIZED_PROXIES`: the ranges of allowed connection IPs, as reported by the socket
+
+Note: `ASGI_AUTHORIZED_PROXIES` is typically the IP range for the reverse proxy, that populates
+the forwarded HTTP request with the `x-real-ip` header.
+
+Example:
+
+    export ASGI_AUTHORIZED_NETWORKS="142.251.0.0/16 143.0.0.0/24"
+    export ASGI_AUTHORIZED_PROXYS="10.0.0.0/24 127.0.0.0/24"
 
 Add the following lines to a file `sitecustomize.py` accessible through the python path:
 
@@ -37,6 +45,7 @@ logger = get_logger()
 
 class AsgiIpFilteringMiddleware:
 
+    ROUTES_REQUIRING_FILTERING = ["anonymous", "invited", "authenticated", "ws"]
     ENV_VAR_NAME_NETWORK = "ASGI_AUTHORIZED_NETWORKS"
     ENV_VAR_NAME_PROXY = "ASGI_AUTHORIZED_PROXIES"
     MESSAGE_REJECTED = (
@@ -51,12 +60,15 @@ class AsgiIpFilteringMiddleware:
         authorized_proxies: Optional[str] = None,
     ):
         """
-        Authorized networks are provided as a string of IPv4 or IPv6 networks
-        (e.g `192.168.0.0/16` or `2001:db00::0/24`) separated with whitespace
-        (spaces, tabs or newlines).
+        Authorized networks and proxies are provided as a string of IPv4 or IPv6 networks
+        (e.g `192.168.0.0/16` or `2001:db00::0/24`) separated with whitespace (spaces,
+        tabs or newlines).
 
-        If the `authorized_networks` argument is not provided the environment
-        variable `ASGI_AUTHORIZED_NETWORKS` is used.
+        If the `authorized_networks` argument is not provided, the environment variable
+        `ASGI_AUTHORIZED_NETWORKS` is used.
+
+        Similarly, if the `authorized_proxies` argument is not provided, the environment
+        variable `ASGI_AUTHORIZED_PROXIES` is used.
         """
         self.asgi_app = asgi_app
         if authorized_networks is None:
@@ -101,38 +113,56 @@ class AsgiIpFilteringMiddleware:
             return False
         return any(proxy_ip in proxy for proxy in self.authorized_proxies)
 
+    def path_requires_ip_filtering(self, path: str) -> bool:
+        """
+        Return `True` if the route requires IP checking `False` otherwise.
+        """
+        try:
+            empty, route_type, *_ = path.split("/")
+        except ValueError:
+            return False  # Not a route we're meant to check
+        if empty != "":
+            return False  # Can this even happen?
+        return route_type in self.ROUTES_REQUIRING_FILTERING
+
     async def __call__(
         self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
     ) -> None:
         """
         ASGI entry point for new connections.
         """
-        if scope["type"] in ("http", "websocket"):
-            scope = cast(Union[HTTPScope, WebsocketScope], scope)
-            client = scope.get("client")
-            if client is None:
-                logger.info("No client information is provided", **scope)
-                return await self.http_reject(scope, send)
-            ip_proxy, _ = client
-            if not self.is_proxy_authorized(ip_proxy):
-                logger.info("A connection has been rejected", **scope)
-                return await self.http_reject(scope, send, ip_proxy)
-            headers = scope.get("headers")
-            if headers is None:
-                logger.info("No header information is provided", **scope)
-                return await self.http_reject(scope, send)
-            # headers type is Iterable[Tuple[bytes, bytes]]
-            x_real_ip = [(field, value) for (field, value) in headers if field == b"x-real-ip"]
-            if not len(x_real_ip):
-                logger.info("No x-real-ip information is provided", **scope)
-                return await self.http_reject(scope, send)
-            _, ip_host = x_real_ip[0]
-            if ip_host is None:
-                logger.info("No x-real-ip information is provided", **scope)
-                return await self.http_reject(scope, send)
-            if not self.is_network_authorized(ip_host.decode()):
-                logger.info("A connection has been rejected", **scope)
-                return await self.http_reject(scope, send, ip_host.decode())
+        # Ignore "lifespan" calls
+        if scope["type"] not in ("http", "websocket"):
+            return await self.asgi_app(scope, receive, send)
+
+        # Ignore routes that do not require filtering
+        scope = cast(Union[HTTPScope, WebsocketScope], scope)
+        if not self.path_requires_ip_filtering(scope["path"]):
+            return await self.asgi_app(scope, receive, send)
+
+        # Check that client info is provided
+        client = scope.get("client")
+        if client is None:
+            logger.info("No client information is provided", **scope)
+            return await self.http_reject(scope, send)
+
+        # Check that the proxy is authorized
+        ip_proxy, _ = client
+        if not self.is_proxy_authorized(ip_proxy):
+            logger.info("A connection has been rejected", **scope)
+            return await self.http_reject(scope, send, ip_proxy)
+
+        # Check that the `x-real-ip` header is provided
+        x_real_ip = dict(scope["headers"]).get(b"x-real-ip")
+        if x_real_ip is None:
+            logger.info("No x-real-ip information is provided", **scope)
+            return await self.http_reject(scope, send)
+
+        # Check that the network is authorized
+        ip_host = x_real_ip.decode()
+        if not self.is_network_authorized(ip_host):
+            logger.info("A connection has been rejected", **scope)
+            return await self.http_reject(scope, send, ip_host)
 
         return await self.asgi_app(scope, receive, send)
 
@@ -191,17 +221,50 @@ try:
     import pytest
     from quart import websocket
     from quart.testing.connections import WebsocketDisconnectError
+    from quart.typing import TestClientProtocol
     from quart_trio import QuartTrio
 except ImportError:
     pass
 else:
 
-    @pytest.mark.trio
-    async def test_asgi_ip_filtering() -> None:
+    @pytest.fixture
+    def test_client() -> TestClientProtocol:
         app = QuartTrio(__name__)
 
         @app.route("/")
-        async def http_route() -> str:  # type: ignore[misc]
+        async def base_route() -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/administration")
+        async def administration_route() -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/administration/<arg>")
+        async def administration_route_with_arg(arg: str) -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/anonymous")
+        async def anonymous_route() -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/anonymous/<arg>")
+        async def anonymous_route_with_arg(arg: str) -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/invited")
+        async def invited_route() -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/invited/<arg>")
+        async def invited_route_with_arg(arg: str) -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/authenticated")
+        async def authenticated_route() -> str:  # type: ignore[misc]
+            return "Hello World"
+
+        @app.route("/authenticated/<arg>")
+        async def authenticated_route_with_arg(arg: str) -> str:  # type: ignore[misc]
             return "Hello World"
 
         @app.websocket("/ws")
@@ -209,56 +272,167 @@ else:
             await websocket.accept()
             await websocket.send("WS event")
 
+        @app.websocket("/ws/<arg>")
+        async def ws_route_with_arg(arg: str) -> None:  # type: ignore[misc]
+            await websocket.accept()
+            await websocket.send("WS event")
+
         app.asgi_app = AsgiIpFilteringMiddleware(app.asgi_app, "130.0.0.0/24 131.0.0.0/24", "127.0.0.0/24 128.0.0.0/24")  # type: ignore[assignment]
+        return app.test_client()
 
-        client = app.test_client()
+    TEST_FILTERED_HTTP_ROUTES = (
+        "/anonymous",
+        "/invited",
+        "/authenticated",
+        "/anonymous/test",
+        "/invited/test",
+        "/authenticated/test",
+    )
 
-        # Regular route
-        response = await client.get(
-            "/",
-            scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]},
+    TEST_UNFILTERED_HTTP_ROUTES = (
+        "/",
+        "/administration",
+        "/administration/test",
+    )
+
+    TEST_WS_ROUTES = (
+        "/ws",
+        "/ws/test",
+    )
+
+    @pytest.mark.trio
+    @pytest.mark.parametrize("route", TEST_FILTERED_HTTP_ROUTES + TEST_UNFILTERED_HTTP_ROUTES)
+    async def test_asgi_ip_filtering_http_with_insider_ip(
+        test_client: TestClientProtocol, route: str
+    ) -> None:
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("127.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"130.0.0.1")],
+            },
         )
         assert response.status_code == 200
         assert (await response.data).decode() == "Hello World"
 
-        response = await client.get(
-            "/",
-            scope_base={"client": ("128.0.0.1", 1234), "headers": [(b"x-real-ip", b"131.0.0.1")]},
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("128.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"131.0.0.1")],
+            },
         )
         assert response.status_code == 200
         assert (await response.data).decode() == "Hello World"
 
-        response = await client.get(
-            "/",
-            scope_base={"client": ("129.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]},
+    @pytest.mark.trio
+    @pytest.mark.parametrize("route", TEST_UNFILTERED_HTTP_ROUTES)
+    async def test_asgi_ip_filtering_unfiltered_http_with_outsider_ip(
+        test_client: TestClientProtocol, route: str
+    ) -> None:
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("129.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"130.0.0.1")],
+            },
+        )
+        assert response.status_code == 200
+        assert (await response.data).decode() == "Hello World"
+
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("128.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"132.0.0.1")],
+            },
+        )
+        assert response.status_code == 200
+        assert (await response.data).decode() == "Hello World"
+
+    @pytest.mark.trio
+    @pytest.mark.parametrize("route", TEST_FILTERED_HTTP_ROUTES)
+    async def test_asgi_ip_filtering_filtered_http_with_outsider_ip(
+        test_client: TestClientProtocol, route: str
+    ) -> None:
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("129.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"130.0.0.1")],
+            },
         )
         assert response.status_code == 403
         assert response.content_type == "text/html; charset=UTF-8"
         expected = AsgiIpFilteringMiddleware.MESSAGE_REJECTED.format("129.0.0.1")
         assert (await response.data).decode() == expected
 
-        response = await client.get(
-            "/",
-            scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"132.0.0.1")]},
+        response = await test_client.get(
+            route,
+            scope_base={
+                "client": ("127.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"132.0.0.1")],
+            },
         )
         assert response.status_code == 403
         assert response.content_type == "text/html; charset=UTF-8"
         expected = AsgiIpFilteringMiddleware.MESSAGE_REJECTED.format("132.0.0.1")
         assert (await response.data).decode() == expected
 
+    @pytest.mark.trio
+    async def test_asgi_ip_filtering_unknown_route(test_client: TestClientProtocol) -> None:
+        response = await test_client.get(
+            "/unknown",
+            scope_base={
+                "client": ("127.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"130.0.0.1")],
+            },
+        )
+        assert response.status_code == 404
+
+        response = await test_client.get(
+            "/unknown",
+            scope_base={
+                "client": ("128.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"131.0.0.1")],
+            },
+        )
+        assert response.status_code == 404
+
+        response = await test_client.get(
+            "/unknown",
+            scope_base={
+                "client": ("129.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"130.0.0.1")],
+            },
+        )
+        assert response.status_code == 404
+
+        response = await test_client.get(
+            "/unknown",
+            scope_base={
+                "client": ("128.0.0.1", 1234),
+                "headers": [(b"x-real-ip", b"132.0.0.1")],
+            },
+        )
+        assert response.status_code == 404
+
+    @pytest.mark.trio
+    @pytest.mark.parametrize("route", TEST_WS_ROUTES)
+    async def test_asgi_ip_filtering_websocket(test_client: TestClientProtocol, route: str) -> None:
         # Websocket route
-        async with client.websocket("/ws", scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]}) as ws:  # type: ignore[call-arg]
+        async with test_client.websocket(route, scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]}) as ws:  # type: ignore[call-arg]
             assert await ws.receive() == "WS event"
 
-        async with client.websocket("/ws", scope_base={"client": ("128.0.0.1", 1234), "headers": [(b"x-real-ip", b"131.0.0.1")]}) as ws:  # type: ignore[call-arg]
+        async with test_client.websocket(route, scope_base={"client": ("128.0.0.1", 1234), "headers": [(b"x-real-ip", b"131.0.0.1")]}) as ws:  # type: ignore[call-arg]
             assert await ws.receive() == "WS event"
 
         with pytest.raises(WebsocketDisconnectError) as ctx:
-            async with client.websocket("/ws", scope_base={"client": ("129.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]}) as ws:  # type: ignore[call-arg]
+            async with test_client.websocket(route, scope_base={"client": ("129.0.0.1", 1234), "headers": [(b"x-real-ip", b"130.0.0.1")]}) as ws:  # type: ignore[call-arg]
                 await ws.receive()
         assert ctx.value.args == (403,)
 
         with pytest.raises(WebsocketDisconnectError) as ctx:
-            async with client.websocket("/ws", scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"132.0.0.1")]}) as ws:  # type: ignore[call-arg]
+            async with test_client.websocket(route, scope_base={"client": ("127.0.0.1", 1234), "headers": [(b"x-real-ip", b"132.0.0.1")]}) as ws:  # type: ignore[call-arg]
                 await ws.receive()
         assert ctx.value.args == (403,)
